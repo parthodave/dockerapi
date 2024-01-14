df400db870a956492a00c32f1a8aae275d7b30fa
parthodave <parthodave@gmail.com>
2024-01-14 17:40:39 +0530

    initial commit

---
+++ StudentApp/Data/StudentAppContext.cs
@@ -0,0 +1,45 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using StudentApp.Models;
+
+namespace StudentApp.Data
+{
+    public class StudentAppContext : DbContext
+    {
+        public StudentAppContext()
+        {
+            
+        } 
+        public StudentAppContext (DbContextOptions<StudentAppContext> options)
+            : base(options)
+        {
+            try
+            {
+                var databaseCreater = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if (databaseCreater != null)
+                {
+                    if (!databaseCreater.CanConnect())
+                    {
+                        databaseCreater.Create();
+                    }
+                    if (!databaseCreater.HasTables())
+                    {
+                        databaseCreater.CreateTables();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public DbSet<StudentApp.Models.Student> Student { get; set; }
+    }
+}
